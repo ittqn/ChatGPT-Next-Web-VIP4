48c4bfd8bfe8949a6185b80bc52e23c51d387b61
itxtl <tianlehl@163.com>
2023-07-08 01:37:04 +0800

    Update constant.ts
    fix: 自定义修改

--- app/constant.ts
+++ app/constant.ts
@@ -7,7 +7,7 @@ export const RELEASE_URL = `${REPO_URL}/releases`;
 export const FETCH_COMMIT_URL = `https://api.github.com/repos/${OWNER}/${REPO}/commits?per_page=1`;
 export const FETCH_TAG_URL = `https://api.github.com/repos/${OWNER}/${REPO}/tags?per_page=1`;
 export const RUNTIME_CONFIG_DOM = "danger-runtime-config";
-export const DEFAULT_API_HOST = "https://vip4.autoai.ink";
+export const DEFAULT_API_HOST = "https://chatgpt1.nextweb.fun/api/proxy";
 
 export enum Path {
   Home = "/",
@@ -78,15 +78,15 @@ export const DEFAULT_MODELS = [
   },
   {
     name: "gpt-4-32k",
-    available: false,
+    available: true,
   },
   {
     name: "gpt-4-32k-0314",
-    available: false,
+    available: true,
   },
   {
     name: "gpt-4-32k-0613",
-    available: false,
+    available: true,
   },
   {
     name: "gpt-3.5-turbo",
